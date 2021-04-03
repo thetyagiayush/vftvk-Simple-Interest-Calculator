08d7a7c4cbe741eaa4ba9db696bfc3c7f37ae7a9
Ayush Tyagi <thetyagiayush@gmail.com>
2021-04-03 19:52:39 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    if(principal <= 0){
+        alert("Enter a Positve Number")
+        document.getElementById("principal").focus()
+        
+    }
+    
+
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+    var yearToString = parseInt(years, 10);
+    NoOFYears = yearToString + 2021;
+
+    document.getElementById("result").innerHTML = "If you deposit " + principal +",<br>at an interest rate of " +rate + "%.<br>You will receive an amount of " + interest +",<br>in the year " + NoOFYears;
+}
+    
+function rangeRelay(){
+    var range =  document.getElementById("rate").value;
+    document.getElementById("range_result").innerHTML = range +"%";
+
+    
+
     
 }
-        
+
